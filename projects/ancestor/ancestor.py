1accc0dd0c617a84671c57347c832c20fd6162ea
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2020-01-21 13:01:36 -0800

    added graphand util to the ancestor file

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,11 @@
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    count=1
+    graph=Graph()
+    for i in ancestors:
+        graph.add_vertex(i[0])
+    print(graph.vertices)
+    for i in ancestors:
+        graph.add_edge(i[0],i[1])
+    print(graph.dfs(1, 10))
