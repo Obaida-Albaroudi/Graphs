55a13d0b41ebcdcd58f1197cc6965b882595e3bd
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2020-01-21 13:48:16 -0800

    Passing first two tests

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,11 +1,30 @@
 from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    count=1
     graph=Graph()
     for i in ancestors:
-        graph.add_vertex(i[0])
-    print(graph.vertices)
+        for x in i:
+            graph.add_vertex(x)
     for i in ancestors:
         graph.add_edge(i[0],i[1])
-    print(graph.dfs(1, 10))
+        # graph.add_edge(i[1],i[0])
+    shortest=[None]*len(ancestors)
+    node=starting_node
+    for i in graph.vertices:
+        print("hi",i)
+        if i!=starting_node and graph.bfs(i,starting_node):
+            print("INSIDE",i)
+            if len(graph.bfs(i,starting_node)) == len(shortest):
+                if i < node:
+                    shortest=graph.bfs(i,starting_node)
+                    starting_node=i
+            if len(graph.bfs(i,starting_node)) < len(shortest):
+                shortest=graph.bfs(i,starting_node)
+                starting_node=i
+        print("Wow", i,starting_node)
+    if node==starting_node:
+        starting_node=-1
+    return starting_node
+
+        
+    # print("WOOOOW",shortest)
